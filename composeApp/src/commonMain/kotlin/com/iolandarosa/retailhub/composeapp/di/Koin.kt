3ea2e716dcaae746be19146f0539967b7e94f5c3
iolandarosa <iolanda.rosa@voidsoftware.com>
2026-08-20 14:44:38 +0100

    retail-hub-4: Add composeApp tests

--- composeApp/src/commonMain/kotlin/com/iolandarosa/retailhub/composeapp/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/com/iolandarosa/retailhub/composeapp/di/Koin.kt
@@ -4,11 +4,10 @@ import com.iolandarosa.retailhub.core.network.di.networkModule
 import com.iolandarosa.retailhub.features.auth.di.authModule
 import org.koin.core.context.startKoin
 
+val appModules = listOf(
+    networkModule,
+    authModule,
+)
 fun initKoin() {
-    startKoin {
-        modules(
-            networkModule,
-            authModule,
-        )
-    }
+    startKoin { modules(appModules) }
 }
