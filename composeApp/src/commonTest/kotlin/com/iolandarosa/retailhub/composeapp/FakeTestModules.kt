783c8d97b890260682cb078dedb0a721badd61fe
iolandarosa <iolanda.rosa@voidsoftware.com>
2026-08-28 14:30:23 +0100

    Remove unused imports

--- composeApp/src/commonTest/kotlin/com/iolandarosa/retailhub/composeapp/FakeTestModules.kt
+++ composeApp/src/commonTest/kotlin/com/iolandarosa/retailhub/composeapp/FakeTestModules.kt
@@ -1,10 +1,7 @@
 package com.iolandarosa.retailhub.composeapp
 
 import com.iolandarosa.retailhub.core.datastore.domain.TokenManager
-import com.iolandarosa.retailhub.core.model.AuthTokens
 import dev.mokkery.mock
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 import org.koin.dsl.module
 
 val fakeTestModule = module {
